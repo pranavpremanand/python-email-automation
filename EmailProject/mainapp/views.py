66f89bf5412448cba48d17526eaa7150133deea7
pranav-aapkacfo <pranavpremanand@aapkacfo.com>
2023-09-18 13:40:33 +0530

    completed celery basic setup and completed sending mail

---
+++ EmailProject/mainapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .tasks import test_func
+from django.http import HttpResponse
+from send_mail.tasks import send_mail_func
+
+# Create your views here.
+def test(request):
+    test_func.delay()
+    return HttpResponse('Done')
+
+def send_mail_to_all(request):
+    send_mail_func.delay()
+    return HttpResponse('Sent')
---
+++ EmailProject/send_mail/tasks.py
@@ -0,0 +1,18 @@
+from django.contrib.auth import get_user_model
+from celery import shared_task
+from django.core.mail import send_mail
+from EmailProject import settings
+
+@shared_task(bind=True)
+def send_mail_func(self):
+    # users = get_user_model.objects.all()
+    # for user in users:
+    mail_subject = 'Celery Testing'
+    message = 'Hello Good morning'
+    to_mail = 'mpranavprem@gmail.com'
+    send_mail(subject=mail_subject,
+              message=message,
+              from_email=settings.EMAIL_HOST_USER,
+              recipient_list=[to_mail],
+              fail_silently=True)
+    return "Done"
